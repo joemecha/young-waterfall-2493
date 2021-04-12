f9685a833b326917828c6d40c4fa2f4b77ad38ff
joemecha <jwmecha@gmail.com>
2021-04-12 10:28:15 -0500

    Add routes for studios and movies using resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "studios#index"
+
+  resources :studios do
+    resources :movies, only: [:show]
+  end
 end
